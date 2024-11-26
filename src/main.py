7560eb2dba69c4ece50fd534c42b136d5f138c56
Bartlomiej Szolkowski <bszolkowski123@gmail.com>
2024-11-26 17:53:22 +0100

    Add functions for retrieving master+ game ids

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
 from dotenv import load_dotenv
 import os
 import json
+from datetime import datetime
 from utils.riot_api import (
     get_match_result,
     get_puuid_by_riot_id,
     get_player_matches_ids,
     get_match_timeline,
+    get_puuid_by_summoner_id,
+    get_apex_tiers_summoner_ids,
+    get_account_by_puuid
 )
-
 
 load_dotenv()
 
 API_KEY = os.getenv("RIOT_API")
-
 
 # Example Usage:
 if __name__ == "__main__":
@@ -23,22 +25,41 @@ if __name__ == "__main__":
     puuid = get_puuid_by_riot_id(
         api_key=API_KEY, region=region, game_name=riot_username, tag=tag
     )
-    print(f"PUUID from Riot ID: {puuid}")
+    # print(f"PUUID from Riot ID: {puuid}")
 
     # Get Match IDs by PUUID
     match_ids = get_player_matches_ids(api_key=API_KEY, region=region, puuid=puuid)
-    print(f"Match IDs: {match_ids}")
+    # print(f"Match IDs: {match_ids}")
 
     last, *_ = match_ids
 
     match_result = get_match_result(api_key=API_KEY, region=region, match_id=last)
-    print(f"Match Result: {match_result}")
+    # print(f"Match Result: {match_result}")
     with open("match_result.json", "w") as f:
         json.dump(match_result, f, indent=4)
 
     # Get Match Timeline by Match ID
     match_timeline = get_match_timeline(api_key=API_KEY, region=region, match_id=last)
-    print(f"Match Timeline: {match_timeline}")
+    # print(f"Match Timeline: {match_timeline}")
 
     with open("match_timeline.json", "w") as f:
         json.dump(match_timeline, f, indent=4)
+
+    # ===============================================================
+    platform = "euw1"  # League of Legends server, not Riot ID region
+    region = 'europe'
+
+    # all master+ EUW player's summoner_ids
+    apex_tiers_summoner_ids = get_apex_tiers_summoner_ids(API_KEY, platform=platform)
+    with open("apex_tiers_summoner_ids.json", "w") as f:
+        json.dump(apex_tiers_summoner_ids, f, indent=4)
+
+    with open('apex_tiers_summoner_ids.json') as f:
+        data = json.load(f)
+        summoner = data[0]
+        start_time = datetime(year=2024, month=11, day=22)  # request match id's of games finished after given date
+
+        puuid = get_puuid_by_summoner_id(api_key=API_KEY, platform=platform, summoner_id=summoner['summonerId'])
+        print(f"player info: {get_account_by_puuid(api_key=API_KEY, region=region, puuid=puuid)}")
+        match_ids = get_player_matches_ids(api_key=API_KEY, region=region, puuid=puuid, start_time=start_time)
+        print(match_ids)
--- src/utils/riot_api.py
+++ src/utils/riot_api.py
@@ -2,6 +2,11 @@ from dotenv import load_dotenv
 import os
 from PoroPilot import PoroPilot
 import requests
+from datetime import datetime
+
+QUEUE_ID = 420  # id of soloQ queue type (queue id's at https://static.developer.riotgames.com/docs/lol/queues.json)
+QUEUE = 'RANKED_SOLO_5x5'  # soloQ
+GAME_TYPE = "ranked"
 
 
 def get_puuid_by_riot_id(api_key, region, game_name, tag):
@@ -17,10 +22,21 @@ def get_puuid_by_riot_id(api_key, region, game_name, tag):
         return None
 
 
-def get_player_matches_ids(api_key, region, puuid):
-    url = (
-        f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids"
-    )
+def get_puuid_by_summoner_id(api_key, platform, summoner_id):
+    url = f"https://{platform}.api.riotgames.com/lol/summoner/v4/summoners/{summoner_id}"
+    headers = {"X-Riot-Token": api_key}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("puuid")
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return None
+
+
+def get_account_by_puuid(api_key, region, puuid):
+    url = f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-puuid/{puuid}"
     headers = {"X-Riot-Token": api_key}
     response = requests.get(url, headers=headers)
 
@@ -30,6 +46,48 @@ def get_player_matches_ids(api_key, region, puuid):
     else:
         print(f"Error: {response.status_code}, {response.text}")
         return None
+
+
+def get_player_matches_ids(api_key, region, puuid, queue=QUEUE_ID, game_type=GAME_TYPE, count: int = None,
+                           start_time: datetime = None, end_time: datetime = None):
+    url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids"
+    headers = {"X-Riot-Token": api_key}
+    params = {
+        "queue": queue,
+        "type": game_type,
+    }
+    if count:
+        params["count"] = count
+    if start_time:
+        params["startTime"] = round(start_time.timestamp())
+    if end_time:
+        params["endTime"] = round(end_time.timestamp())
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return None
+
+
+def get_apex_tiers_summoner_ids(api_key, platform):
+    headers = {"X-Riot-Token": api_key}
+    apex_leagues = ['challengerleagues', 'grandmasterleagues', 'masterleagues']
+
+    summoner_ids = []
+    for league in apex_leagues:
+        url = f"https://{platform}.api.riotgames.com/lol/league/v4/{league}/by-queue/{QUEUE}"
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()['entries']
+            summoner_ids.extend([{'summonerId': summoner['summonerId']} for summoner in data])
+        else:
+            print(f"Error: {response.status_code}, {response.text}")
+            break
+    return summoner_ids
 
 
 def get_match_result(api_key, region, match_id):
